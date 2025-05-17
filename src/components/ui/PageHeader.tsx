92e15e2a21500f0b240f6293fabb813ac94db309
freelancewithrandhir <freelancewithrandhir@gmail.com>
2025-05-18 00:19:32 +0530

    Update PageHeader.tsx

--- src/components/ui/PageHeader.tsx
+++ src/components/ui/PageHeader.tsx
@@ -6,8 +6,8 @@ const PageHeader = ({ title, subtitle }: { title: string; subtitle?: string }) =
         {subtitle && <p className="text-xl text-blue-100 max-w-2xl mx-auto">{subtitle}</p>}
       </div>
       
-      {/* Wave Divider */}
-      <div className="absolute bottom-0 left-0 right-0">
+       {/* Wave Divider Intitial className="absolute bottom-0 left-0 right-0" */}
+      <div className="absolute">
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 120" className="w-full h-auto">
           <path fill="#ffffff" fillOpacity="1" d="M0,96L80,85.3C160,75,320,53,480,58.7C640,64,800,96,960,101.3C1120,107,1280,85,1360,74.7L1440,64L1440,120L1360,120C1280,120,1120,120,960,120C800,120,640,120,480,120C320,120,160,120,80,120L0,120Z"></path>
         </svg>
